8a2506ee379b0e087eff2a4eb4a02b789a0417ef
yalpha <clement.naissans@gmail.com>
2026-03-11 13:25:52 +0100

    perf: utilisation poolSize et options Mongoose pour limiter la saturation

--- src/db/db.js
+++ src/db/db.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, /*{
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }*/);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            minPoolSize: 5,
+            maxPoolSize: 20
+        });
         console.log('✅ MongoDB connected');
     } catch (err) {
         console.error('❌ MongoDB connection error:', err);
